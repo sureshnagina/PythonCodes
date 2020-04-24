72a8e4ee6a7c5e6adb2e8259cb324648046209b8
sureshnagina <37480957+sureshnagina@users.noreply.github.com>
2020-04-23 20:57:25 -0400

    Math Ops code push

---
+++ MathOps.py
@@ -0,0 +1,18 @@
+#Maths Operations
+integerNumber=int(input("Enter the Integer Number :" ))
+floatNumber=float(input("Enter the Float Numbere : "))
+complexNumber=complex(input( "Enter the complex Number :"))
+#Sum of two numbers
+print(" Sum of Integer and complex number is :" , integerNumber+complexNumber)
+#Difference of two numbers
+print(" Difference of float and complex number is :" , floatNumber-complexNumber)
+#Multiplication of two numbers
+print("Multiplication of complex and float number is :", complexNumber*floatNumber)
+#Reminder of two numbers when divided
+print("Reminder of Float number when divided by integer number is :", floatNumber%integerNumber)
+#Division of two numbers
+print("Division of integer with float number is :", integerNumber*floatNumber)
+#Exponential caluculation
+print("Exponential of integer with float number is :" ,floatNumber**integerNumber)
+#Floor Division
+print("Floor division of float and integer number is: " ,floatNumber//integerNumber)
